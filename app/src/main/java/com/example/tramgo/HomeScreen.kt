4ef8ffc31d7302f6418ca47a6caf536675d9f361
anroszkiewicz <111358119+anroszkiewicz@users.noreply.github.com>
2024-06-13 22:37:55 +0200

    added tram animation to home screen

--- app/src/main/java/com/example/tramgo/HomeScreen.kt
+++ app/src/main/java/com/example/tramgo/HomeScreen.kt
@@ -1,14 +1,19 @@
 package com.example.tramgo
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -36,6 +41,9 @@ fun HomeScreen (
 ) {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
+        var visible by remember { mutableStateOf(false) }
+        val density = LocalDensity.current
+
         Column(
             modifier = Modifier
                 .padding(24.dp)
@@ -49,6 +57,22 @@ fun HomeScreen (
                 fontSize = 30.sp,
                 textAlign = TextAlign.Center
             )
+
+            LaunchedEffect(key1 = Unit, block = {
+                delay(200L)
+                visible = true
+            })
+            AnimatedVisibility(
+                visible = visible,
+                enter =
+                slideInVertically(animationSpec = tween(durationMillis = 2000)) + scaleIn(animationSpec = tween(durationMillis = 2000))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.drawing),
+                    contentDescription = "logo",
+                    modifier = Modifier.size(600.dp)
+                )
+            }
         }
     }
 }
